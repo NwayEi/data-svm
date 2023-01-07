8abe9a9d059e94f4a9102bd007e3f3296ee7e3e7
orchid aung <orchid.lso@gmail.com>
2023-01-07 15:03:25 +0800

    change variable to avoid badstyle

--- utils/plots.py
+++ utils/plots.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import ListedColormap
 
-def plot_decision_regions(_x, _y, classifier, test_idx=None):
+def plot_decision_regions(_x, _y, classifier, test_idx=None, resolution=0.02):
     """
     Plot the 2D-decision region of a classifier
     with matplotlib along its first two dimensions X[:,0] and X[:,1].
@@ -18,22 +18,13 @@ def plot_decision_regions(_x, _y, classifier, test_idx=None):
         resolution (float, optional): Resolution of the
         meshgrid used to colorize regions. Defaults to 0.02.
     """
-    resolution=0.02
+
     # setup marker generator and color map up for up to 5 classes
     #markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(_y))])
 
-    # plot the decision surface
-    x1_min, x1_max = _x[:, 0].min() - 1, _x[:, 0].max() + 1
-    x2_min, x2_max = _x[:, 1].min() - 1, _x[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                           np.arange(x2_min, x2_max, resolution))
-    _z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    _z = _z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, _z, alpha=0.4, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
+    plot_contourf(_x,resolution,classifier,cmap)
 
     # Plot samples
     for idx, _cl in enumerate(np.unique(_y)):
@@ -49,3 +40,15 @@ def plot_decision_regions(_x, _y, classifier, test_idx=None):
                         alpha=1, color=cmap(idx),
                         linewidths=1, marker='o', s=55, label=f'test {_cl}')
     plt.legend()
+
+def plot_contourf(_x, resolution, classifier, cmap):
+    # plot the decision surface
+    x1_min, x1_max = _x[:, 0].min() - 1, _x[:, 0].max() + 1
+    x2_min, x2_max = _x[:, 1].min() - 1, _x[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    _z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    _z = _z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, _z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
